6c1ace5873e28adc4ea13621b4857759635c6b31
Akira FUNAI <akira@funai.com>
2011-08-03 01:08:42 +0900

    dynamically load images from Twitpic.

--- tipi.rb
+++ tipi.rb
@@ -1,6 +1,23 @@
 require 'rubygems'
+require 'net/http'
+require 'rexml/document'
 require 'sinatra'
+require 'erubis'
 
-get '/' do
-  'hi!'
+get '/:username' do
+
+  url = "http://api.twitpic.com/2/users/show.xml?username=#{params[:username]}"
+  xml = Net::HTTP.get_response(URI.parse url).body
+  doc = REXML::Document.new xml
+
+  images = doc.elements.collect('user/images/image') do |image|
+    {
+      :short_id  => image.elements['short_id'].text,
+      :message   => image.elements['message'].text,
+      :timestamp => image.elements['timestamp'].text,
+    }
+  end
+
+  erubis :carousel, :locals => {:images => images}
+
 end
